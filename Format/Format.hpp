c1302afe8ac5303fba7ff749072c1a9ba9e3fbc9
bodong <dbdongbo@vip.qq.com>
2021-12-07 14:46:36 +0800

    main header.

--- Format/Format.hpp
+++ Format/Format.hpp
@@ -1,5 +1,20 @@
 // FormatLibrary 
 // @author bodong 
+/*
+this simple project is a C++ format library, it use .net style to format string. if you have used C++/CLI or C#, you will be very familiar with this code:
+
+string str;
+StandardLibrary::FormatTo(str, "{0}--#--{1,8}--#--{2}", 100, -40.2f, " String ");
+
+and then, str is:
+100--#--  -40.20--#-- String
+
+this library is a cross-platform libary, i have compiled it under windows, macos, linux with msvc, g++ and xcode darwin C++ with c++ 11 support or with out c++ 11 support. 
+and in most case, it will be 3x fast than c sprintf and it is thread safe.
+this project design like boost, it is a header only project. if you want to use it, you only have to include the main Format.hpp.
+
+@github:https://github.com/sczybt/CPPFormatLibrary
+*/
 
 // Main Entry
 #pragma once
