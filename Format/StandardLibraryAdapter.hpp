e8760dec26e8ad62da2cefdd2136ade4994cdcb0
bodong <bodong@tencent.com>
2024-06-21 09:41:39 +0800

    delete old case

--- Format/StandardLibraryAdapter.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// include standard library policy
-#include <Format/Details/StandardLibrary/StandardPatternPolicy.hpp>
-
-// include stl format support
-#include <Format/Details/StandardLibrary/FormatTo.hpp>
