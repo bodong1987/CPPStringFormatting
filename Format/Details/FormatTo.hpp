0de959f2bb2db244b77f3d88ca8018f2c6e456a1
bodong1987 <dbdongbo@vip.qq.com>
2024-07-10 09:51:18 +0800

    Revert "临时备份"
    This reverts commit a820db2a213ff05e5cacfb756738d4ef539115a5.

--- Format/Details/FormatTo.hpp
+++ Format/Details/FormatTo.hpp
@@ -32,9 +32,40 @@ namespace Formatting
 {
     namespace Details
     {
-        FL_CONSTEXPR inline size_t CalculateByteArrayHash(const uint8_t* s, size_t count)
+        constexpr inline size_t CalculateByteArrayHash(const uint8_t* const start, const size_t length)
         {
-            return count ? (CalculateByteArrayHash(s, count - 1) ^ s[count - 1]) * 16777619u : 2166136261u;
+#if FL_PLATFORM_X64
+            FL_STATIC_ASSERT(sizeof(void*) == 8, "This code is for 64-bit pointer.");
+
+            constexpr size_t FNVOffsetBasis = 14695981039346656037ULL;
+            
+            // ReSharper disable once CppTooWideScope
+            constexpr size_t FNVPrime = 1099511628211ULL;
+
+#else
+            FL_STATIC_ASSERT(sizeof(void*) == 4, "This code is for 32-bit pointer.");
+
+            constexpr SizeType FNVOffsetBasis = 2166136261U;
+
+            // ReSharper disable once CppTooWideScope
+            constexpr SizeType FNVPrime = 16777619U;
+#endif
+
+            size_t Value = FNVOffsetBasis;
+            
+            for (size_t Next = 0; Next < length; ++Next)
+            {
+                // fold in another byte
+                Value ^= static_cast<size_t>(start[Next]);
+                Value *= FNVPrime;
+            }
+
+#if FL_PLATFORM_X64
+            FL_STATIC_ASSERT(sizeof(void*) == 8, "This code is for 64-bit pointer.");
+            Value ^= Value >> 32;
+#endif
+            
+            return Value;
         }
         
 #if FL_COMPILER_IS_GREATER_THAN_CXX11
